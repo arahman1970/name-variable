3cd0d341360b6af862c164750406b037f1ab810d
arahman1970 <ar.nj.usa@gmail.com>
2019-06-13 10:01:11 -0400

    var

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+// A program that assigns name to a variable and prints the variable to the console
+public class Main {
+
+    public static void main(String[] args) {
+	String name;
+    name="Abdul Rahman";
+	System.out.println("My Name is" +" " + name);
+    }
+}
